6b5e9056f104faf0463f357d7621b98b17fbf939
Nicholas Clement <Nicholas.clement@colorado.edu>
2016-08-23 22:51:21 -0600

    started winning alg

--- Tic Tac Toe/Tic Tac Toe.py
+++ Tic Tac Toe/Tic Tac Toe.py
@@ -1,19 +1,31 @@
 #simple tic tac toe game
 
-numberSpaces = int(input("Enter your number of spaces."))
+numberSpaces = 0 #int(input("Enter your number of spaces."))
 
 x = 0
 
 def sideSpace():
-    print("___" * numberSpaces)
+    print(" ___" * numberSpaces)
 
 def verticalBar():
-    print("|  " * numberSpaces)
+    print("|   " * (numberSpaces + 1))
+
 
 
 while x < numberSpaces:
     sideSpace()
     verticalBar()
-
     x += 1
 sideSpace()
+
+
+def threeInRow(scoreList):
+    print(scoreList[0][0], scoreList[1][0], scoreList[2][0])
+    #if scoreList[0,]
+
+myList = [[0,1,2], [0,1,2], [0,1,2]]
+
+print (myList)
+
+
+threeInRow(myList)
