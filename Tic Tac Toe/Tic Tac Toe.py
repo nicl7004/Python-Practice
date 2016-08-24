224a1ef0abfd3c4af43ea05ff6489b554453ca68
Nicholas Clement <Nicholas.clement@colorado.edu>
2016-08-24 15:25:06 -0600

    just need to finish diag

--- Tic Tac Toe/Tic Tac Toe.py
+++ Tic Tac Toe/Tic Tac Toe.py
@@ -20,63 +20,65 @@ sideSpace()
 
 
 def threeInRow(scoreList):
-    y = 1
-    x = 0
-    vert = 1
-    while x < (numberSpaces -1):
-        print("first while")
-        if 1==1:
-            while y < (numberSpaces -1):
-                print("second while")
-                print("x is",x,"y is",y)
-                if scoreList[x][y-1] == scoreList[x][y]:
-                    print("vertical match")
-                    #print(scoreList[x][y])
-                    vert += 1
-                    print("Vertical count", vert)
-                y += 1
+    x=0
+    for x in range(len(scoreList)):
+        vertMatches = verticalWin(scoreList,x)
+        horizMatches = horizWin(scoreList, x)
+        if (horizMatches) != 0:
+            break
+        elif (vertMatches) != 0:
+            break
+        x+=1
 
-        x += 1
-        if vert == numberSpaces:
-            print("Winner")
-# verticalWin finds if all the cells in the current column are the same value and returns 1 if this is true
-def verticalWin(scoreList, column):
+    return horizMatches, vertMatches, diagWin(scoreList)
+# # verticalWin finds if all the cells in the current column are the same value and returns 1 if this is true
+def horizWin(scoreList, column):
     vert = 1
     for i in range(len(scoreList)-1):
         if scoreList[column][i]==scoreList[column][i+1]:
             vert += 1
-            print("Vert =", vert)
-        else:
-            break
-    if vert == (len(scoreList)):
-        return 1
-    else:
-        return 0
-
-def horizWin(scoreList, row):
+            #print("Vert =", vert)
+            if vert == len(scoreList):
+                return [1, scoreList[column][i]]
+    return 0
+#horizWin finds if all the cells in the current row are the same value and returns (1, value in cell)
+def verticalWin(scoreList, row):
     horiz = 1
     for i in range(len(scoreList)-1):
         if scoreList[i][row]==scoreList[i+1][row]:
             horiz += 1
-            print("horiz = ", horiz)
+            #print("horiz = ", horiz)
             if horiz == len(scoreList):
-                return 1, scoreList[i][row]
+                return [1, scoreList[i][row]]
+    return 0
+
+def diagWin(scoreList):
+    diag = 1
+    for i in range(len(scoreList)-1):
+        if scoreList[i][i] == scoreList[i+1][i+1]:
+            diag += 1
+
+            if diag == len(scoreList):
+                return[1, scoreList[0][0]]
+    return 0
 
 
 
 
+myList = [[0,1,0],
+          [1,2,0],
+          [0,1,0]]
+score = threeInRow(myList)
 
-
-
-myList = [[0,1,0], [1,1,1], [0,1,2]]
-print (verticalWin(myList,1))
-print (horizWin(myList,1))
-
-# print (myList)
-# threeInRow(myList)
-# print (myList[0][0], myList[0][1])
-# #
-# # print (myList)
-# #
-# #
-# # threeInRow(myList)
+#print("Horiz wins =", score[0])
+if score[0] != 0:
+    print("Horiz win = true")
+    print("Horiz win number", score[0][1])
+#print("Vert wins =", score[1])
+if score[1] != 0:
+    print("Vert win = true")
+    print("Vert wins number", score[1][1])
+#print("Diag wins =", score[2])
+if score[2] != 0:
+    print("Diag win = true")
+    print("Diag wins number", score[2][1])
