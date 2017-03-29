008b248df5e232ec261841c24fc454e829c46579
Nicl7004 <nicholas.clement@colorado.edu>
2017-03-29 16:15:43 -0600

    linked list and basic hashtable

---
+++ bst/linkedList.py
@@ -0,0 +1,61 @@
+class linkedList:
+    def __init__(self,data):
+        self.value = data
+        self.next = None
+
+class linkedListOperation:
+    def insertEnd(self,head,key,value):
+        if head.next != None:
+            self.insertEnd(head.next,key,value)
+        else:
+            head.next = linkedList((key,value))
+
+    def printList(self,head):
+        if head.next is not None:
+            print(head.value)
+            self.printList(head.next)
+        else:
+            print(head.value)
+class hashtable:
+    def __init__(self,size):
+        self.array = [None] * size
+        self.size = size
+
+    def hashFunction(self,size,key):
+        return (key%size) # return the index to be inserted at
+
+    def insertInto(self,key,value):
+        index = self.hashFunction(self.size, key)
+
+        # Check if collision at that location in array:
+        if self.array[index] is None:
+            self.array[index] = linkedList((key,value))
+        else:
+            insertLinked = linkedListOperation()
+            insertLinked.insertEnd(self.array[index], key, value)
+
+    def printTable(self):
+        for intEntry,entry in enumerate(self.array):
+            if entry is not None:
+                print("@ Index %d " % intEntry)
+                linkedListOperation().printList(entry)
+
+def main():
+    head = linkedList((23,10))
+    head.next = linkedList((24,11))
+    head.next.next = linkedList((34,15))
+    operation = linkedListOperation()
+    operation.printList(head)
+    print("\n\n")
+    operation.insertEnd(head,1100,69)
+    operation.printList(head)
+    print("\n\n\n")
+    table = hashtable(100)
+
+    table.insertInto(239,"Hello")
+    table.insertInto(139, "hello2")
+    table.insertInto(112, "Inserted at 112 fam this is an entry")
+    table.printTable()
+
+if __name__ == '__main__':
+    main()
