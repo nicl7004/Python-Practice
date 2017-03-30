f0ded61e784a0d0235fd7e2690d3dd5b616cbc51
Nicl7004 <nicholas.clement@colorado.edu>
2017-03-29 18:49:16 -0600

    deleting all except head

--- bst/linkedList.py
+++ bst/linkedList.py
@@ -16,6 +16,18 @@ class linkedListOperation:
             self.printList(head.next)
         else:
             print(head.value)
+
+    def delete(self, prev, head, key, value):
+        if head != None:
+            if head.value == (key,value) and prev == None:
+                # print("Deleting head of ll", head.value, prev, head.next)
+                self.delete(None, head.next, key, value)
+            elif head.value == (key,value):
+                prev.next = head.next
+            else:
+                prev = head
+                self.delete(prev, head.next, key, value)
+
 class hashtable:
     def __init__(self,size):
         self.array = [None] * size
@@ -26,7 +38,6 @@ class hashtable:
 
     def insertInto(self,key,value):
         index = self.hashFunction(self.size, key)
-
         # Check if collision at that location in array:
         if self.array[index] is None:
             self.array[index] = linkedList((key,value))
@@ -41,17 +52,23 @@ class hashtable:
                 linkedListOperation().printList(entry)
 
 def main():
+    #linkedList
     head = linkedList((23,10))
     head.next = linkedList((24,11))
     head.next.next = linkedList((34,15))
     operation = linkedListOperation()
     operation.printList(head)
-    print("\n\n")
+    print("\n\n---------------INSERT----------------")
     operation.insertEnd(head,1100,69)
     operation.printList(head)
-    print("\n\n\n")
-    table = hashtable(100)
+    print("\n\n---------------DELETE 23,10 && 24,11----------------")
+    operation.delete(None,head,23,10)
+    operation.delete(None,head,24,11)
+    operation.printList(head)
 
+    print("\n\n\n---------------HASHTABLE----------------")
+    #table
+    table = hashtable(100)
     table.insertInto(239,"Hello")
     table.insertInto(139, "hello2")
     table.insertInto(112, "Inserted at 112 fam this is an entry")
